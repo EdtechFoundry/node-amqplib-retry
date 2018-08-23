751defe8764f83dadda61fba9aa649c736e2b28a
Are Almaas <mralmaas@gmail.com>
2018-08-23 10:21:37 +0200

    feat: add edtech foundry node-logger (#3)
    Adding node-logger as peer dependency to able to view the logs in google cloud.

--- src/ready_queue_consumer.js
+++ src/ready_queue_consumer.js
@@ -1,27 +1,30 @@
-const config = require('./config')
-const Promise = require('bluebird')
+const config = require('./config');
+const Promise = require('bluebird');
+const { Log } = require('@edtechfoundry/node-logger');
 
 class ReadyQueueConsumer {
-  constructor (channel) {
-    this.channel = channel
+  constructor(channel) {
+    this.channel = channel;
   }
 
-  start () {
-    const self = this
-    return self.channel.consume(config.readyQueueName, (msg) =>
-      Promise
-        .try(() => {
-          const targetQueueName = msg.properties.headers._targetQueue
-          const properties = msg.properties.headers._originalProperties
-          return self.channel.sendToQueue(targetQueueName, new Buffer(msg.content), properties)
-        })
+  start() {
+    const self = this;
+    return self.channel.consume(config.readyQueueName, msg =>
+      Promise.try(() => {
+        const targetQueueName = msg.properties.headers._targetQueue;
+        const properties = msg.properties.headers._originalProperties;
+        return self.channel.sendToQueue(targetQueueName, new Buffer(msg.content), properties);
+      })
         .then(() => self.channel.ack(msg))
-        .catch((err) => {
-          console.error('Error: while trying to process message from ready queue.  err: ' + err + ', msg: ' + JSON.stringify(msg))
-          self.channel.nack(msg)
+        .catch(err => {
+          Log.error(
+            'Error: while trying to process message from ready queue. msg: ' + JSON.stringify(msg),
+            err
+          );
+          self.channel.nack(msg);
         })
-    )
+    );
   }
 }
 
-module.exports = ReadyQueueConsumer
+module.exports = ReadyQueueConsumer;
